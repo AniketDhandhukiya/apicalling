3e0a0e0f66f68b2a5e4da47ff22fa6dc785bb1a5
AniketDhandhukiya <addhandhukiya2309@gmail.com>
2023-03-26 12:38:36 +0530

    co

---
+++ apicalling/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  apicalling
+//
+//  Created by R83 on 16/03/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lb1: UILabel!
+    @IBOutlet weak var lb2: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- apicalling/ViewController.swift
+++ apicalling/ViewController.swift
@@ -1,6 +1,7 @@
 
 
 import UIKit
+import Alamofire
 
 
 struct api: Codable{
@@ -10,33 +11,78 @@ struct api: Codable{
     var body: String
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
 
+    @IBOutlet weak var datatable: UITableView!
     var arr : [api] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getApi()
     }
+    
 
     
-    func getApi(){
+
+    
+    func getApi() {
         let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
         var ur = URLRequest(url: url!)
         ur.httpMethod = "GET"
-        
+
         URLSession.shared.dataTask(with: ur) { data, response, error in
             do{
-                if error == nil{
+            if error == nil{
                     self.arr = try JSONDecoder().decode([api].self, from: data!)
-                    print(self.arr)
+//                    print(self.arr)
+                    DispatchQueue.main.async {
+                        self.datatable.reloadData()
+                    }
                 }
             }
             catch{
                 print(error.localizedDescription)
             }
-        }.resume()
+          }.resume()
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = datatable.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.lb1.text = "\(arr[indexPath.row].id)"
+        cell.lb2.text = arr[indexPath.row].title
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+//    func getApi(){
+//        AF.request("https://jsonplaceholder.typicode.com/posts",method: .get).responseData { response in
+//            switch response.result{
+//            case .success(let data):
+//                do {
+//                    if data == response.value{
+//                        self.arr = try JSONDecoder().decode([api].self     , from: data)
+//                        DispatchQueue.main.async {
+//                            self.datatable.reloadData()
+//                        }
+//                    }
+//                }
+//                catch {
+//                    print(error.localizedDescription)
+//                }
+//
+//
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//
+//        }
+//
+//    }
 }
 
